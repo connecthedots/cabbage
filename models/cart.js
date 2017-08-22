e67e7eea51dd89f82e9400086c2ff5723f80c10d
Wendy <zhaod.wendy@gmail.com>
2017-08-22 12:12:14 -0700

    Add shopping cart model and route to add item to cart

--- models/cart.js
+++ models/cart.js
@@ -1,18 +1,43 @@
 //Exports constructor function, creates shopping cart object
 module.exports = function Cart(cartItems){
-	this.cart = cartItems;
+	this.basket = cartItems;
 	this.totalQty = 0;
 	this.totalPrice = 0;
-	//each time we add an item, we take old version of shopping cart
+
+	//quick way to check if cart is empty
+	console.log(Object.keys(cartItems).length !==0)
+	if (Object.keys(cartItems).length !==0) {
+        for (var key in cartItems) {
+            this.totalQty += cartItems[key].aggQuantity;
+            this.totalPrice += cartItems[key].aggPrice;
+            console.log("==========" + this.totalQty)
+            console.log("==========" + this.totalPrice)
+        }
+    }
+	//each time we add a product, we take previous version of shopping cart
 	//look for the item in the shopping cart
 	//if it's not in the cart, add it as a new item
 	//if it's already in the cart, only increase the quantity of that item
 	this.add = function(product, productId){
-		var product = this.cart[productId];
+		//aggItem is the aggregate of units stored for this product
+		var aggItem = this.basket[productId]
 		//Product not in current shopping cart
-		if (!product){
-			this.cart[productId] = {item: product, quantity: 0, price: 0} 
+		if (!this.basket[productId]){
+			this.basket[productId] = {"productModel": product, "aggQuantity": 0, "aggPrice": 0} 
 		}
+		this.basket[productId].aggQuantity++;
+		this.basket[productId].aggPrice = this.basket[productId].productModel.price * this.basket[productId].aggQuantity;
+		this.totalQty++;
+		this.totalPrice += this.basket[productId].productModel.price;
 
 	}
-}
+
+    this.makeArray = function() {
+        var arr = [];
+        for (var id in this.basket) {
+        	//push each aggregated product to array
+            arr.push(this.basket[productId]);
+        }
+        return arr;
+    };
+};
--- routes/product.js
+++ routes/product.js
@@ -1,6 +1,7 @@
 const express = require("express"),
 	router = express.Router(),
-	Products = require("../models/product")
+	Products = require("../models/product"),
+	Cart = require("../models/cart")
 
 //Seeding in sample data to db
 Products.remove({}, function(err, reply){
@@ -49,8 +50,27 @@ router.get("/", function(req,res){
 });
 
 router.get("/add/:productId", function(req, res){
-	var productId = req.params.productId
-	res.send("This route adds a product to the shopping cart with id: " + productId);
+	var productId = req.params.productId;
+	Products.findById(productId, function(err, product){
+		if (err){
+			console.log(err);
+			res.redirect("/shop");
+		} else {
+			//checks if there is a cart in sessions already,
+			//If yes, set the basket of the previous cart as starting basket
+			//If not, assign empty object as starting basket
+			var createdCart = new Cart(req.session.cart ? req.session.cart.basket : {});
+			//add the product to cart
+			createdCart.add(product, product._id);
+			//save cart to session
+			req.session.cart = createdCart;
+			console.log("New cart created, and product added");
+			console.log(req.session.cart);
+			res.redirect("/shop");
+		}
+	});
 });
+
+
 
 module.exports = router
